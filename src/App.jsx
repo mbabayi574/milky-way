396aac21bb7473048b0eece3f9e65fd0b472bcd9
Bobby <M.Babati574@Gmail.Com>
2025-09-08 12:49:48 +0330

    In The Name Of God

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>سلام‌علیکم. الحمدالله!</h1>
+      </div>
+      <h1>تیتر ۱</h1>
+    </>
+  );
+}
+
+export default App;
